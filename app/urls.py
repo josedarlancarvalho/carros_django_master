8136d337222f5bbb681c052cbb7546590ab0efb9
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-12 18:02:48 -0300

    organizando view carros

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from cars.views import cars_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_views),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
