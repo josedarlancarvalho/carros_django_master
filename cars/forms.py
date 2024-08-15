c30d524c270a91c3e1c6797ee9a3265f5ec8af64
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-15 14:39:55 -0300

    corrigindo bug preço

--- cars/forms.py
+++ cars/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from cars.models import Car
 
-
 class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
-        
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value is not None and value < 20000:
+            self.add_error('value', 'O valor mínimo do carro deve ser R$20.000')
+        return value
