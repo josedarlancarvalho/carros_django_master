8e14007faa9ace27f6c4197f3fb82fb5e56f5617
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-24 17:11:46 -0300

    adicionando validação do ano

--- cars/forms.py
+++ cars/forms.py
@@ -11,3 +11,9 @@ class CarModelForm(forms.ModelForm):
         if value is not None and value < 20000:
             self.add_error('value', 'O valor mínimo do carro deve ser R$20.000')
         return value
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get("factory_year")
+        if factory_year < 1975:
+            self.add_error("factory_year", "nao é possivel cadastrar carros antes de 1975")
+            return factory_year
