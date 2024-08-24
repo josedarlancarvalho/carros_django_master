b1405e17bb092ad0b65c31f237500a192e35d387
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-24 17:28:00 -0300

    criando pasta base da template do login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 
 def register_view(request):
-    a
+    user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_form})
