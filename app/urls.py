ffc2f137b75e2885b858981326c6bdf752e56b06
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-25 21:06:30 -0300

    add detail e update view

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-from cars.views import CarsListView, NewCarCreateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -13,4 +13,6 @@ urlpatterns = [
     path("logout/", logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView,DetailView,UpdateView
     
 class CarsListView(ListView):
     model = Car
@@ -20,4 +20,14 @@ class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
+    success_url = '/cars/'
+    
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
     success_url = '/cars/'
