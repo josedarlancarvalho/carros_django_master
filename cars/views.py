b50beae75eef282f4f5c871a97c561013dd1a7d4
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-26 16:04:27 -0300

    ajustando a carupdateview

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView,DetailView,UpdateView, DeleteView
     
 class CarsListView(ListView):
@@ -30,7 +31,10 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
-    success_url = '/cars/'
+    
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+    
     
 class CarDeleteView(DeleteView):
     model = Car
